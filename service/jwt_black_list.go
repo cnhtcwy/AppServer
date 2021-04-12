1335757132bed3034b4a363f24a907d55bbc52d8
wangyang <35348462@qq.com>
2021-04-12 08:32:07 +0800

    修改jwt黑名单插入操作

--- service/jwt_black_list.go
+++ service/jwt_black_list.go
@@ -13,8 +13,8 @@ import (
 //@return: err error
 
 func JsonInBlacklist(jwtList model.JwtBlacklist) (err error) {
-	sqlStr := "insert into jwt_blacklists(created_at, jwt) values (?,?)"
-	_,err = global.GVA_DB.Exec(sqlStr, time.Time{}, jwtList.Jwt)
+	sqlStr := "insert into jwt_blacklists(created_at,updated_at, jwt) values (now(),now(),?)"
+	_, err = global.GVA_DB.Exec(sqlStr, jwtList.Jwt)
 	return err
 }
 
@@ -27,11 +27,11 @@ func JsonInBlacklist(jwtList model.JwtBlacklist) (err error) {
 func IsBlacklist(jwt string) bool {
 	var i int
 	sqlStr := "select count(*) from jwt_blacklists where jwt =?"
-	err := global.GVA_DB.Get(&i,sqlStr,jwt)
-	if err!=nil {
+	err := global.GVA_DB.Get(&i, sqlStr, jwt)
+	if err != nil {
 		return false
 	}
-	if i>0 {
+	if i > 0 {
 		return true
 	}
 	return false
